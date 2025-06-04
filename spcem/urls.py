09aaf8f11ecc035981861ca44aec9bbc24b89312
theokyle <theokyle@gmail.com>
2025-06-04 16:28:07 -0500

    add block number pathing

--- spcem/urls.py
+++ spcem/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("block/", views.BlockView.as_view(), name="block"),
+    path("block/", views.AllBlockView.as_view(), name="all_block"),
+    path("block/<int:block_number>/", views.BlockView.as_view(), name="block"),
     path("list/", views.BlockListView.as_view(), name="list")
 ]
--- spcem/views.py
+++ spcem/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views.generic import ListView
 from .models import Owner, Grave, Deceased
 
 # Create your views here.
-class BlockView (ListView):
+class AllBlockView (ListView):
     model = Grave
     template_name = "spcem/block.html"
 
+class BlockView (ListView):
+    template_name = "spcem/block.html"
+
+    def get_queryset(self):
+        return Grave.objects.filter(block = self.kwargs["block_number"])
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["block_number"] = self.kwargs["block_number"]
+        return context
+    
 class BlockListView (ListView):
     model = Grave
     template_name = "spcem/list.html"
